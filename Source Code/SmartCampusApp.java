2c8b755ecf46eff54c4fe610d417cea715c405bc
Ethan Williams <74257529+EthanBW23@users.noreply.github.com>
2025-04-30 01:31:13 +0100

    Delete Source Code/SmartCampusApp.java

--- Source Code/SmartCampusApp.java
+++
@@ -1,8 +0,0 @@
-package Smart_Campus_Management_System;
-
-// Starting point of the Smart Campus Management System
-public class SmartCampusApp {
-    public static void main(String[] args) { // Main method
-        new mainWindow(); // Launches the mainWindow GUI
-    }
-}
